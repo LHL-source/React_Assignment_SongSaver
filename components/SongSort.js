3668a7286586a6203c7068ec63cb1cbcdd4a383e
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-11-11 12:45:36 +0100

    Add files via upload

---
+++ components/SongSort.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function SongSort ({sortering, handleClickSort}) {
+    // console.log("entering SongSort");
+    // console.log(`Entering SongSort`);
+    return (
+
+        <form className="songsort">
+            <p>
+              How to sort the songs?  
+            </p>
+
+        <select 
+        name="songsort"
+        //value={sortering.value}
+        onChange={handleClickSort}
+        >
+            <option value="">--choose please an item---</option>
+            <option value="TAZ">title a-z</option>
+            <option value="TZA">title z-a</option>
+            <option value="AAZ">artist a-z</option>
+            <option value="AAZ">artist a-z</option>
+            <option value="SHL">stars 5-1</option>
+            <option value="SLH">starts 1-5</option>
+           
+        </select>
+        </form>
+    )//return
+
+}//function
+
+
+export default SongSort;
