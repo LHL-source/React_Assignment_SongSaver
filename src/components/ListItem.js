2bafaec53a1306f8f0529b03e44091e3aa95de25
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-11-28 09:47:18 +0100

    Add files via upload

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,11 +3,13 @@ import React from "react";
 function ListItem ({ song, genre}){
     return (
         <li className="listcontainer">
-            <p>{song.id}</p>
-            <p>{song.title}</p>
-            <p>{song.artist}</p>
+          <div className="ttz">
+            <p >{song.id}</p>
+            <p >{song.title}</p>
+            <p >{song.artist}</p>
             <p>{song.genre}</p>
             <p>{song.rating}</p>
+            </div>
         </li>
     )
 
